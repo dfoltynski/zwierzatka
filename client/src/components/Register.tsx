4ec151f58a3577869fb1bf2bb142baa5b4c32f01
Dawid Foltyński <dawid@dawids-air-3.home>
2021-02-26 22:51:35 +0100

    name validation

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -16,6 +16,7 @@ export default function Login() {
   const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
   const [name, setName] = useState<string>("");
+  const [validName, setValidName] = useState<boolean>(true);
 
   const [email, setEmail] = useState<string>("");
   const [validEmail, setValidEmail] = useState<boolean>(true);
@@ -43,15 +44,27 @@ export default function Login() {
     return false;
   };
 
+  const validateName = (name: string): boolean => {
+    if (name.trim().split(" ").length >= 2) return true;
+
+    setValidName(false);
+    return false;
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.formTextInput}
+        style={validName ? styles.formTextInput : styles.invalidText}
         placeholder="John Doe"
-        onChangeText={(name) => setName(name)}
+        onChangeText={(name) => {
+          if (validateName(name)) {
+            setValidName(true);
+            setName(name);
+          }
+        }}
       />
       <TextInput
-        style={validEmail ? styles.formTextInput : styles.invalidEmail}
+        style={validEmail ? styles.formTextInput : styles.invalidText}
         placeholder="johndoe@example.com"
         onChangeText={(email) => {
           if (validateEmail(email)) {
@@ -116,7 +129,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
 
-  invalidEmail: {
+  invalidText: {
     fontSize: 24,
 
     borderColor: "#fc034e",
